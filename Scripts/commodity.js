b7dbb5a52bf44c67bc41d3a26dbf97a68d3403d5
Srinivas Borigarla <iamborigarla@Srinivass-MacBook-Pro.local>
2023-02-16 22:53:49 +0530

    added commodity.js

--- Scripts/commodity.js
+++ Scripts/commodity.js
@@ -3,9 +3,17 @@ import fetch from "node-fetch";
 
 // const AUTH_TOKEN = "wwapcfucann9a4v598xd2956rh7c5w31844zr6hk35y455srd50srd48n0vf"
 
+var base_currency = 'USD'
+var symbol = 'RICE' 
+var endpoint = 'latest'
+var access_key = 'lq98djv5r050inmagtp1ne3e5cegmdi333e7z4i6209co5utj1lp84pt2ydo'
+
+
 async function commodityPrice() {
-    const url = "https://commodities-api.com/api/latestPost"
-	const data = await fetch(url).then(response => response.json())
-    console.log(data);
+    var url = 'https://commodities-api.com/api/'+endpoint+'?access_key='+access_key+'&base='+base_currency+'&symbols='+symbol
+	var data = await fetch(url).then(response => response.json())
+    // price in cwt (1 / price)
+    var price = Math.round(1 / data['data']['rates'][symbol])
+    console.log(price)
 }
 commodityPrice()
