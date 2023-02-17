088c846965ca699a77430e8de1255f9de284eb1d
Srinivas Borigarla <iamborigarla@Srinivass-MacBook-Pro.local>
2023-02-17 20:10:55 +0530

    Added swaping functions

--- Scripts/bicSwap.js
+++ Scripts/bicSwap.js
@@ -9,6 +9,7 @@ const signer = new ethers.Wallet(process.env.SIGNER_PRIVATE_KEY, provider);
 
 const bicSwapABI = [
     "function swapExactInputSingle(uint)",
+    "function swapExactOutputSingle(uint, uint)",
   ]
 const bicTokSwap = "0x6D4d1563AFBb4509f42c3b718B71675275C7C410";
 
@@ -16,8 +17,18 @@ const bicTokSwap = "0x6D4d1563AFBb4509f42c3b718B71675275C7C410";
 // call methods
 async function main() {
     const signercontract = new ethers.Contract(bicTokSwap, bicSwapABI, signer);
-    const singlswap = await signercontract.swapExactInputSingle("100000000000000000");
-    console.log(singlswap);
+    
+    // Each LINK has 18 decimal places
+    // const link = ethers.utils.parseUnits("0.1", 18);
+    // const singlswap = await signercontract.swapExactInputSingle("100000000000000000");
+    
+    
+    // 1 BIC token
+    const amountOut = "1000000000000000000";
+    // 10 Link tokens
+    const amountInMaximum = "10000000000000000000";
+    const exactOutput = await signercontract.swapExactOutputSingle("100000000000000000", amountInMaximum)
+    console.log(exactOutput);
 }
 
 main();
